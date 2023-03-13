ebb6b69e094a167ad1d66d835880f8d5f4fd13cf
EugeniaGramajo <gramajoeugenia@gmail.com>
2023-03-13 16:01:04 -0300

    fix url

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ import Details from "./components/details";
 import Login from "./components/login";
 import Register from "./components/register"
 import axios from "axios";
-axios.defaults.baseURL = "https://videogames-pi-eg.onrender.com/videogames"
+axios.defaults.baseURL = "https://videogames-pi-eg.onrender.com/"
 
 function App() {
   const dispatch = useDispatch();
