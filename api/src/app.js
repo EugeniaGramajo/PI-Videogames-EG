4251e1e0fa1bb3dc65a04d6a26b179a18f5b8d45
EugeniaGramajo <gramajoeugenia@gmail.com>
2023-03-13 00:19:55 -0300

    change url

--- api/src/app.js
+++ api/src/app.js
@@ -10,7 +10,7 @@ require('./db.js');
 const server = express();
 
 server.name = 'API';
-server.use(cors({credentials:true, origin:"*"}))
+server.use(cors({credentials:true, origin:"https://pi-videogames-eg.vercel.app"}));
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
