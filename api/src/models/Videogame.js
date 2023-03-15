5f4203fca9d583308b0014d64a75a64c3d015564
EugeniaGramajo <gramajoeugenia@gmail.com>
2023-03-15 12:42:20 -0300

    platfom => platforms

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -25,7 +25,7 @@ module.exports = (sequelize) => {
     rating: {
       type: DataTypes.FLOAT
     },
-    platform: {
+    platforms: {
       type: DataTypes.ARRAY(DataTypes.STRING),
       allowNull: false
     },
